9b1fe06ce6c40860240a6506ec4a34889dc1eaf4
Jan Bureš <jan.bures@live.com>
2023-04-26 04:55:48 +0200

    First version of library mod template

---
+++ SpaceWarp.Template/templates/SpaceWarpModLibrary/scripts/build-release.bat
@@ -0,0 +1,2 @@
+@echo off
+dotnet build SpaceWarpModLibrary.sln -c Release
---
+++ SpaceWarp.Template/templates/SpaceWarpModLibrary/src/SpaceWarpModLibrary/SpaceWarpModLibraryPlugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using HarmonyLib;
+using KSP.UI.Binding;
+using SpaceWarp;
+using SpaceWarp.API.Assets;
+using SpaceWarp.API.Mods;
+using SpaceWarp.API.Game;
+using SpaceWarp.API.Game.Extensions;
+using SpaceWarp.API.UI;
+using SpaceWarp.API.UI.Appbar;
+using UnityEngine;
+
+namespace SpaceWarpModLibrary;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency(SpaceWarpPlugin.ModGuid, SpaceWarpPlugin.ModVer)]
+public class SpaceWarpModLibraryPlugin : BaseSpaceWarpPlugin
+{
+    // These are useful in case some other mod wants to add a dependency to this one
+    // ReSharper disable UnusedMember.Global
+    public const string ModGuid = MyPluginInfo.PLUGIN_GUID;
+    public const string ModName = MyPluginInfo.PLUGIN_NAME;
+    public const string ModVer = MyPluginInfo.PLUGIN_VERSION;
+    // ReSharper restore UnusedMember.Global
+
+    // Singleton instance of the plugin class
+    // ReSharper disable once UnusedAutoPropertyAccessor.Global
+    public static SpaceWarpModLibraryPlugin Instance { get; set; }
+
+    /// <summary>
+    /// Runs when the mod is first initialized.
+    /// </summary>
+    public override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Instance = this;
+    }
+}
