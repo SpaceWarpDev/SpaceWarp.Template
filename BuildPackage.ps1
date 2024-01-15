467e0fd2f9a3838b3cee690025f6e0535fdb19d2
Jan Bureš <jan.bures@live.com>
2024-01-15 21:31:03 +0100

    This *should* work

--- BuildPackage.ps1
+++ BuildPackage.ps1
@@ -64,10 +64,9 @@ Write-Host "Packing NuGet package"
 if ($IsWindows) {
     $nugetCommand = "nuget"
 } else {
-    $nugetCommand = "mono", "/usr/local/bin/nuget.exe"
+    $nugetCommand = "mono /usr/local/bin/nuget.exe"
 }
-$nugetOutput = & $nugetCommand pack "./Package.nuspec" -NoDefaultExcludes -OutputDirectory "$DistFolderPath" `
-    -Properties "NoWarn=NU5110,NU5111;buildDir=$BuildFolderPath" 2>&1
+$nugetOutput = Invoke-Expression "& $nugetCommand pack `"./Package.nuspec`" -NoDefaultExcludes -OutputDirectory `"$DistFolderPath`" -Properties `"NoWarn=NU5110,NU5111;buildDir=$BuildFolderPath`" 2>&1"
 $nugetOutput | ForEach-Object {
     if ($_ -match "SpaceWarp\.Template\.[0-9\.]+\.nupkg") {
         $nugetFileName = $matches[0]
